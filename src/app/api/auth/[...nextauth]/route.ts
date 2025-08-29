22c72e18049b44549778c7e49206d436409be931
Jakub Kruczek <131014483+Rufus785@users.noreply.github.com>
2025-08-29 19:00:33 +0200

    .

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,6 @@
 import NextAuth from "next-auth/next";
 import { authOptions } from "@/lib/authOptions";
-import { NextRequest } from "next/server";
 
 const handler = NextAuth(authOptions);
 
-export function GET(req: NextRequest) {
-  return handler(req);
-}
-
-export function POST(req: NextRequest) {
-  return handler(req);
-}
+export { handler as GET, handler as POST };
